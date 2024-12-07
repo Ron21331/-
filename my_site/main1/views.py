1d7c59d1ca0ed046d0ad04f0202031c7625d26c5
Ron21331 <romogeeyt@gmail.com>
2024-12-07 13:53:22 +0300

    Add files via upload

--- my_site/main1/views.py
+++ my_site/main1/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 
-def pusto(request):
-    return render (request, "main1/pusto.html")
+
 def index(request):
     return render (request, "main1/glav.html")
-def vhod(request):
-    return render (request, "main1/vhod.html")
 def carta(request):
     return render (request, "main1/carta.html")
 def res(request):
